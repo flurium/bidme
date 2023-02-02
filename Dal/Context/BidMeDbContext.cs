b5bb4b6de8e97a3f26db31c38886cb364e97d300
mariia-shcherbak <masha.shcherbak.2016@gmail.com>
2023-02-02 20:21:47 +0200

    Context +Model User

---
+++ Dal/Context/BidMeDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal.Context
+{
+    public class BidMeDbContext : IdentityDbContext<User>
+    {
+        public BidMeDbContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Lot> Lots { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+        
+        public DbSet<Order> Orders { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+
+
+    }
+}

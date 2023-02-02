32222339e3274fd1927a6f1a5d3cda3303ed64d6
Roman Koshchei <romankoshchei@gmail.com>
2023-02-02 19:59:45 +0200

    repo

---
+++ Dal/Context/BidMeDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal.Context
+{
+  public class BidMeDbContext : IdentityDbContext
+  {
+  }
+}

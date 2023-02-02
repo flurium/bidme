8019e4f32f56b0380ca45c2e8849c5a90e71108c
DmitryFlud2003 <valvech322@gmail.com>
2023-02-02 20:50:24 +0200

    Lot

---
+++ Domain/Models/Lot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Lot
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+        public ICollection<Grade> Grades { get; set; }
+        public ICollection<LotImage> Images { get; set; }
+    }
+}

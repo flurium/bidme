528f6f9f224614073dd9a12aba3bd85cdefac229
Roman Koshchei <romankoshchei@gmail.com>
2023-02-02 20:30:25 +0200

    sync with maria

--- Dal/Context/BidMeDbContext.cs
+++ Dal/Context/BidMeDbContext.cs
@@ -9,22 +9,22 @@ using System.Threading.Tasks;
 
 namespace Dal.Context
 {
-    public class BidMeDbContext : IdentityDbContext<User>
+  public class BidMeDbContext : IdentityDbContext<User>
+  {
+    public BidMeDbContext(DbContextOptions options) : base(options)
     {
-        public BidMeDbContext(DbContextOptions options) : base(options)
-        {
-            Database.EnsureCreated();
-        }
-
-        public DbSet<Lot> Lots { get; set; }
-
-        public DbSet<Category> Categories { get; set; }
-
-        public DbSet<Order> Orders { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-        }
+      Database.EnsureCreated();
     }
+
+    public DbSet<Lot> Lots { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
+
+    public DbSet<Order> Orders { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      base.OnModelCreating(builder);
+    }
+  }
 }

1e218a9a72e754d9b72370a4cb4072a8f7c255e8
LitvinenkoVitalii <reskator95@gmail.com>
2023-02-02 20:54:09 +0200

    Rating

---
+++ Domain/Models/Rating.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Rating
+    {
+        public double Value { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public bool AsSeller { get; set; }
+
+    }
+}

c9d45c1dbe857dd07d5fc9080129ab2e35757618
LitvinenkoVitalii <reskator95@gmail.com>
2023-02-02 21:23:20 +0200

    Order Rep, Interface, Service, UnitOfWork

---
+++ Dal/Repository/Interfaces/IOrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal.Repository.Interfaces
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+        Task DeleteAsync(string userId, int productId);
+
+        Task<IReadOnlyCollection<Order>> FindIncludeProductsAsync(Expression<Func<Order, bool>> conditon);
+
+        Task Edit(Order order);
+
+        Task<Order?> GetByIdAsync(string userId, int productId);
+    }
+}

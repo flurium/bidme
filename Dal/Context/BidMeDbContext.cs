b80e1a35cd1ea1e93cc89091e85cf73f570b25fa
Roman Koshchei <romankoshchei@gmail.com>
2023-02-04 21:35:03 +0200

    fix database structure by making user in lot nullable field, because if delete seller buyers still want to see what they bought in the past

--- Dal/Context/BidMeDbContext.cs
+++ Dal/Context/BidMeDbContext.cs
@@ -25,18 +25,19 @@ namespace Dal.Context
             base.OnModelCreating(builder);
 
             builder.Entity<Favorite>().HasKey(f => new { f.UserId, f.LotId });
-            builder.Entity<Favorite>().HasOne(f => f.User).WithMany(u => u.Favorites).HasForeignKey(f => f.UserId).OnDelete(DeleteBehavior.NoAction);
+            builder.Entity<Favorite>().HasOne(f => f.User).WithMany(u => u.Favorites).HasForeignKey(f => f.UserId);
             builder.Entity<Favorite>().HasOne(f => f.Lot).WithMany(l => l.Favorites).HasForeignKey(f => f.LotId);
 
             builder.Entity<Order>().HasKey(o => new { o.UserId, o.LotId });
-            builder.Entity<Order>().HasOne(o => o.User).WithMany(u => u.Orders).HasForeignKey(o => o.UserId).OnDelete(DeleteBehavior.NoAction);
-            builder.Entity<Order>().HasOne(o => o.Lot).WithMany(l => l.Orders).HasForeignKey(o => o.LotId).OnDelete(DeleteBehavior.NoAction);
+            builder.Entity<Order>().HasOne(o => o.User).WithMany(u => u.Orders).HasForeignKey(o => o.UserId);
+            builder.Entity<Order>().HasOne(o => o.Lot).WithMany(l => l.Orders).HasForeignKey(o => o.LotId);
 
             builder.Entity<Rating>().HasKey(r => r.Id);
             builder.Entity<Rating>().HasOne(r => r.User).WithMany(u => u.Ratings).HasForeignKey(r => r.UserId);
 
             builder.Entity<Lot>().HasKey(l => l.Id);
             builder.Entity<Lot>().HasOne(l => l.Category).WithMany(c => c.Lots).HasForeignKey(l => l.CategoryId);
+            builder.Entity<Lot>().HasOne(l => l.User).WithMany(u => u.Lots).HasForeignKey(l => l.UserId).IsRequired(false).OnDelete(DeleteBehavior.SetNull);
 
             builder.Entity<LotImage>().HasKey(li => li.Id);
             builder.Entity<LotImage>().HasOne(li => li.Lot).WithMany(l => l.Images).HasForeignKey(l => l.LotId);

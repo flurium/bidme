952b93af478e94ec16dbfce2ec42c375b9fe874d
DmitryFlud2003 <valvech322@gmail.com>
2023-02-07 17:37:19 +0200

    Ban

---
+++ Bll/Models/UserFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bll.Models
+{
+    public class UserFilter
+    {
+        public string Id { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+    }
+}

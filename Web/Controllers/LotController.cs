67fb79a2c1e55b8e1f056ac875e921227e2f2d39
Roman Koshchei <romankoshchei@gmail.com>
2023-02-09 11:13:08 +0200

    not banned as filter

--- Web/Controllers/LotController.cs
+++ Web/Controllers/LotController.cs
@@ -1,7 +1,11 @@
-﻿using Bll.Services;
+﻿using Bll.Models;
+using Bll.Services;
 using Domain.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 using System.Security.Claims;
+using Web.Helpers;
 using Web.Models;
 
 namespace Web.Controllers
@@ -12,22 +16,47 @@ namespace Web.Controllers
 
         private readonly LotService _lotService;
         private readonly CategoryService _categoryService;
-        private readonly LotImageService _lotImageService;
         private readonly IWebHostEnvironment _host;
 
-        public LotController(LotService productService, CategoryService categoryService, LotImageService productImageService, IWebHostEnvironment webHost)
+        public LotController(LotService productService, CategoryService categoryService, IWebHostEnvironment webHost)
         {
             _lotService = productService;
-            _lotImageService = productImageService;
             _categoryService = categoryService;
             _host = webHost;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        /// <summary>
+        /// Main page of filter and serch for lots
+        /// </summary>
+        /// <param name="name">Search name of lot</param>
+        /// <param name="categories">Comma seperated categories names</param>
+        /// <param name="min">Minimal price</param>
+        /// <param name="max">Maximum price</param>
+        public async Task<IActionResult> Index(string? name, double? min, double? max, string categories = "")
+        {
+            var categoryList = categories.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            var catalogViewModel = new CatalogViewModel
+            {
+                Lots = await _lotService.FilterLots(new LotFilter(name, categoryList, min, max)),
+                Categories = await _lotService.GetCategories(categoryList),
+                SelectedCategories = categoryList
+            };
+
+            return View(catalogViewModel);
         }
 
         public async Task<IActionResult> Details(int id)
         {
             var res = await _lotService.GetOne(id);
 
-            if (res == null) return RedirectToAction(nameof(HomeController.Index), HomeController.Name);
+            if (res == null) return RedirectToAction(nameof(LotController.Index), LotController.Name);
 
             if (res.CloseTime <= DateTime.Now && res.IsClosed == false) await _lotService.Expired(res.Id);
 
@@ -35,6 +64,8 @@ namespace Web.Controllers
         }
 
         [HttpGet]
+        [Authorize]
+        [NotBannedAs(Roles = Role.BannedAsSeller)]
         public async Task<IActionResult> Create()
         {
             ViewBag.Categories = await _categoryService.List();
@@ -42,9 +73,12 @@ namespace Web.Controllers
         }
 
         [HttpPost]
+        [Authorize]
+        [NotBannedAs(Roles = Role.BannedAsSeller)]
         public async Task<IActionResult> Create(LotViewModel lotView)
         {
             var time = DateTime.Now;
+
             switch (lotView.CloseTime)
             {
                 case "1":
@@ -59,6 +93,7 @@ namespace Web.Controllers
                     time.AddDays(7.0);
                     break;
             }
+
             Lot lot = new()
             {
                 Name = lotView.Name,
@@ -67,23 +102,23 @@ namespace Web.Controllers
                 Description = lotView.Description,
                 CategoryId = lotView.CategoryId
             };
-            var res = await _lotService.CreateAsync(lot);
+            var res = await _lotService.Create(lot, lotView.Url);
 
-            await _lotImageService.AddToServer(res, lotView.Url);
-
-            return RedirectToAction("RequiredProperty", "Product", new { res.CategoryId, ProductId = res.Id });
+            return RedirectToAction(nameof(LotController.Index));
         }
 
+        [Authorize]
         public async Task<IActionResult> Delete(int id)
         {
-            await _lotImageService.DeleteFromServer(id);
             await _lotService.Delete(id);
             return RedirectToAction("Profile", "Seller");
         }
 
-        public async Task<IActionResult> MakeBid(double bid, int productId)
+        [Authorize]
+        [NotBannedAs(Roles = Role.BannedAsBuyer)]
+        public async Task<IActionResult> MakeBid(double amount, int id)
         {
-            bool res = await _lotService.MakeBid(bid, productId, User.FindFirstValue(ClaimTypes.NameIdentifier));
+            bool res = await _lotService.MakeBid(amount, id, User.FindFirstValue(ClaimTypes.NameIdentifier));
             if (res == false)
             {
                 ViewBag.Error = "The bid must be greater than the price";
@@ -92,10 +127,11 @@ namespace Web.Controllers
             return RedirectToAction("Index");
         }
 
+        [Authorize]
         public async Task<IActionResult> EditDescription(string description, int lotId)
         {
-            var lot = await _lotService.FirstOrDefault(l => l.Id == lotId);
-            if (lot.UserId == User.FindFirstValue(ClaimTypes.NameIdentifier))
+            var lot = await _lotService.GetOne(lotId);
+            if (lot != null && lot.UserId == User.FindFirstValue(ClaimTypes.NameIdentifier))
             {
                 await _lotService.Edit(description, lotId);
             }
---
+++ Web/Helpers/NotBannedAsFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Helpers
+{
+    public class NotBannedAs : AuthorizeAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (Roles != null && context.HttpContext.User.IsInRole(Roles))
+            {
+                context.Result = new ViewResult { ViewName = "Banned" };
+            }
+        }
+    }
+}

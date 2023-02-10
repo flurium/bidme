68e5f2a5878a1e83beedd0346584a1f5cfa3ee5e
LitvinenkoVitalii <reskator95@gmail.com>
2023-02-10 15:35:35 +0200

    Change UserName, Reset Password

---
+++ Web/Models/ResetPasswordViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
